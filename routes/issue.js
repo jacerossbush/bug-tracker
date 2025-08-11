e4eecfbc58581e4a7c47abd15b7448a5e1de55e9
Jace Bush <jacerossbush@gmail.com>
2025-08-11 07:10:08 -0600

    Add Issue Creation route.

--- routes/issue.js
+++ routes/issue.js
@@ -12,4 +12,25 @@ router.get('/all', async (req, res) => {
 	}
 });
 
+router.post('/new', async (req, res) => {
+	const { title, description, assigned_to, status_id, priority, due_date } = req.body;
+	const user = req.user.userId;
+	const assigned = assigned_to ?? user;
+	if (!title || !description || !status_id || !priority || !user || !assigned || !due_date) {
+		return res.status(400).json({ error: 'Missing Required Issue fields.' });
+	}
+	try {
+		const insertResult = await pool.query(
+			`INSERT INTO issues (title, description, created_by, assigned_to, status_id, priority, due_date)
+			 VALUES ($1, $2, $3, $4, $5, $6, $7)
+			 RETURNING id`,
+			[title, description, user, assigned, status_id, priority, due_date]
+		);
+		const newIssueId = insertResult.rows[0].id;
+		res.status(201).json({ message: 'Issue created successfully', id: newIssueId });
+	} catch (err) {
+		res.status(500).json({ error: 'Issue failed to create' });
+	}
+});
+
 module.exports = router;
